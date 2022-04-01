e1d67fcb2ad1e6298dca84a9995eb18c41db7397
lalit bhatt <lalit@rhizicube.ai>
2022-04-01 10:25:15 +0530

    tried connecting to server

--- server.go
+++ server.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"main/chat"
 	"net"
 
 	"google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Go gRPC Beginners Tutorial!")
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 
-	// s := chat.Server{}
+	s := chat.Server{}
 
 	grpcServer := grpc.NewServer()
 
-	// chat.RegisterChatServiceServer(grpcServer, &s)
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
 	}
+
 }
